76b936c7eeaaed126e10686d5bf509920df95f34
ANDRES FELIPE FERNANDEZ GARCIA <affernandez999@gmail.com>
2024-05-15 20:58:06 -0500

    add form in MyComponent

--- src/components/MyComponent/MyComponent.jsx
+++ src/components/MyComponent/MyComponent.jsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function MyComponent() {
-  return <div>MyComponent</div>;
+  const [text, setText] = useState('');
+  const [textSaved, setSaved] = useState('');
+  const textOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const textOnSave = () => {
+    setSaved(text);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={textOnChange} />
+      <button onClick={textOnSave}>Actualizar</button>
+      <p>Texto input: {text}</p>
+      <p>Texto actualizado: {textSaved}</p>
+    </div>
+  );
 }
